bbb24d6636f64679e6b089561636cbd24c198d82
victor nisem <105614421+VICONISEM@users.noreply.github.com>
2024-11-30 17:56:12 +0200

    Done

--- js/dashboard.js
+++ js/dashboard.js
@@ -93,12 +93,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // Update the year dynamically in the footer
     document.getElementById("currentYear").innerText = new Date().getFullYear();
 });
-<<<<<<< HEAD
 
- 
-
-
-=======
 function hideAdminCard() {
     var card = document.getElementById('AdminCard');
     
