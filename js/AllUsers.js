948fd95f99dcfc756d8dc738f0ef30b1f903abe3
AmMaR1922 <ammar191230@gmail.com>
2024-11-30 18:18:39 +0200

    UpdateUseer

--- js/AllUsers.js
+++ js/AllUsers.js
@@ -51,6 +51,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 const updateButton = document.createElement('button');
                 updateButton.classList.add('btn-update');
                 updateButton.textContent = 'Update';
+                window.location.href = 'UpdateUser.html';
                 updateButton.addEventListener('click', function () {
                     handleUpdateUser(user.id);  // You can pass user.id or any other identifier
                 });
